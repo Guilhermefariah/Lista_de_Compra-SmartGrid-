3feb5d8291e8704d77be718a92e5dfdd479d24d5
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-31 15:42:54 -0300

    Container
    adicionado onclick para chamar a funcao addCart

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,16 +19,23 @@ const Page: React.FC = () => {
   ]
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2x1 font-bold mb-4">Test </h1>
-        <div className="grid grid-cols-3 gap-4">
-          
+    <div className="flex justify-center items-center h-screen">
+      <div className="text-white p-4 bg-no-repeat rounded-lg shadow-2xl bg-zinc-800 hover:shadow-black">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="text-3xl font-bold mb-4 text-center">Lista de Compras</h1>
+          <div className="grid grid-cols-3 gap-4">
+            {products.map(product => (
+              <div key={product.id} className="border p-4 rounded-lg">
+                <h2 className="text-lg font-bold">{product.name}</h2>
+                <p className="text-white">${product.price.toFixed(2)}</p>
+                <button onClick={() => addCart(product)} className="bg-rose-600 text-white px-4 py-2 mt-2 rounded-lg">Adicionar ao Carrinho</button>
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </div>
   )
-
-
-
 
 }
 export default Page;
