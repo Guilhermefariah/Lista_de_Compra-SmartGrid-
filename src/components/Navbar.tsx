5636e8cc99ff1b15705f3b92f07939a91021602f
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-06-01 02:38:09 -0300

    Time

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
 
 const Navbar: React.FC = () => {
-  const [timeLeft, setTimeLeft] = useState(600);
+    const [timeLeft, setTimeLeft] = useState(600);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      setTimeLeft(prevTime => (prevTime > 0 ? prevTime - 1 : 0));
-    }, 1000);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft(prevTime => (prevTime > 0 ? prevTime - 1 : 0));
+        }, 1000);
 
-    return () => clearInterval(timer);
-  }, []);
+        return () => clearInterval(timer);
+    }, []);
 
-  const formatTime = (seconds: number) => {
-    const minutes = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
-  };
+    const formatTime = (seconds: number) => {
+        const minutes = Math.floor(seconds / 60);
+        const secs = seconds % 60;
+        return `${minutes}:${secs < 10 ? '0' : ''}${secs}`;
+    };
 
-  return (
-<nav className="fixed top-0 w-full bg-teal-700 p-4 flex justify-between items-center shadow-lg z-50">
-    <img src="/img/mercado.png" alt="Descrição da Logo" className="h-8" />
-  <div className="text-white text-lg">{formatTime(timeLeft)}</div>
-</nav>
-  );
+    return (
+        <nav className="fixed top-0 w-full bg-teal-700 p-2 flex justify-between items-center shadow-lg z-50">
+            <div className="flex items-center">
+                <img src="/img/mercado.png" alt="Logo" className="h-10 w-10" />
+            </div>
+            <div className="flex flex-col items-center flex-grow"> 
+                <div className="text-white text-3xl">{formatTime(timeLeft)}</div>
+                <div className="text-white">Corra! Ofertas limitadas, tempo se esgotando.</div>
+            </div>
+            <div className="flex items-center"> 
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
