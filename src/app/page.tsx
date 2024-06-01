5af45ea821f9961127d6b30043aad8c56fd3b12a
guilhermefaria321@gmail.com <guilhermefaria321@gmail.com>
2024-05-31 23:34:06 -0300

    Matriz

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,11 +21,10 @@ const Page: React.FC = () => {
     { id: 1, name: 'Product 1', price: 10 },
     { id: 2, name: 'Product 2', price: 20 },
     { id: 3, name: 'Product 3', price: 30 },
-    { id: 3, name: 'Product 4', price: 40 },
-    { id: 3, name: 'Product 5', price: 50 },
-    { id: 3, name: 'Product 6', price: 60 }
+    { id: 4, name: 'Product 4', price: 40 },
+    { id: 5, name: 'Product 5', price: 50 },
+    { id: 6, name: 'Product 6', price: 60 }
   ];
-
   return (
     <div className="flex justify-between items-center h-screen">
       <div className="fixed top-4 right-4 z-10 text-lg">
