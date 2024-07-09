3afbf42a864632e1f6edc9c1b5042913f481f657
Guilhermefariah <guilhermefaria321@gmail.com>
2024-07-09 18:02:32 -0300

    update colors

--- src/components/ProductCart.tsx
+++ src/components/ProductCart.tsx
@@ -15,8 +15,8 @@ interface CardProps {
 
 const ProductCart: React.FC<CardProps> = ({ product, onAddCart }) => {
   return (
-    <div className="flex flex-col items-center justify-center bg-orange-200 shadow-gray-200 shadow-lg rounded-xl p-2">
-      <h2 className="text-xl mb-2 font-bold text-gray-600">{product.name}</h2>
+    <div className="flex flex-col items-center justify-center bg-orange-100 shadow-gray-300 shadow-lg rounded-xl p-4">
+      <h2 className="text-xl mb-2 font-bold text-blue-700">{product.name}</h2>
       <div className="relative w-full h-40 max-w-[480px]">
         <Image
           src={product.image}
@@ -26,10 +26,10 @@ const ProductCart: React.FC<CardProps> = ({ product, onAddCart }) => {
           className="rounded-md transition-transform duration-300 opacity-90 ease-in-out hover:scale-110 hover:transform-origin-center"
         />
       </div>
-      <p className="text-xl mt-2 font-bold text-gray-600">R$ {product.price.toFixed(2)}</p>
+      <p className="text-xl mt-2 font-bold text-gray-800">R$ {product.price.toFixed(2)}</p>
       <button
         onClick={() => onAddCart(product)}
-        className="bg-orange-500 text-gray-200 font-bold text-lg px-4 py-2 rounded-lg mt-4 transform transition hover:scale-105"
+        className="bg-green-500 text-white font-bold text-lg px-4 py-2 rounded-lg mt-4 transform transition hover:scale-105"
       >
         Adicionar ao carrinho
       </button>
